0c08dc279ef95b6cec90f6263fd5b37534941fbd
Paul Hottell <paul.hottell@gmail.com>
2017-01-11 10:52:00 -0800

    Sign-in screen completed

---
+++ DevChat/RoundTextField.swift
@@ -0,0 +1,46 @@
+//
+//  RoundTextField.swift
+//  DevChat
+//
+//  Created by Paul Hottell on 1/11/17.
+//  Copyright © 2017 HSCS. All rights reserved.
+//
+
+import Foundation
+
+@IBDesignable
+class RoundTextField: UITextField {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    
+    @IBInspectable var bgColor: UIColor? {
+        didSet {
+            backgroundColor = bgColor
+        }
+    }
+    
+    @IBInspectable var placeholderColor: UIColor? {
+        didSet {
+            let rawString = attributedPlaceholder?.string != nil ? attributedPlaceholder!.string : ""
+            let str = NSAttributedString(string: rawString, attributes: [NSForegroundColorAttributeName: placeholderColor!])
+            attributedPlaceholder = str
+        }
+    }
+}
