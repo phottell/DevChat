1b9e1c2532bdb379bbe228dc27d48e5c89ffe95a
Paul Hottell <paul.hottell@gmail.com>
2017-01-11 16:32:20 -0800

    New user added to Firebase database completed

---
+++ DevChat/DataService.swift
@@ -0,0 +1,27 @@
+//
+//  DataService.swift
+//  DevChat
+//
+//  Created by Paul Hottell on 1/11/17.
+//  Copyright © 2017 HSCS. All rights reserved.
+//
+
+import Foundation
+import FirebaseDatabase
+
+class DataService {
+    private static let _instance = DataService()
+    
+    static var instance: DataService {
+        return _instance
+    }
+    
+    var mainRef: FIRDatabaseReference {
+        return FIRDatabase.database().reference()
+    }
+    
+    func saveUser(uid: String) {
+        let profile: Dictionary<String, AnyObject> = ["firstName": "" as AnyObject, "lastName": "" as AnyObject]
+        mainRef.child("users").child(uid).child("profile").setValue(profile)
+    }
+}
